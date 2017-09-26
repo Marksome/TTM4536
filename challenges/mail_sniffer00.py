406630707da7f86d4e9d640781c8b5422dd2a12d
Markus Andersen <markus.s.andersen@gmail.com>
2017-09-26 13:39:12 +0200

    Added mail_sniffer00.py

---
+++ challenges/mail_sniffer00.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'root'
+from scapy.all import *
+# our packet callback
+def packet_callback(packet):
+	print packet.show()
+# fire up our sniffer
+sniff(prn=packet_callback,count=1)
+
