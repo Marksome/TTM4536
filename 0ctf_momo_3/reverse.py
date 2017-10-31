61fc77d18f565d8120faf550dfcf84d611944ed0
Markus Andersen <markus.s.andersen@gmail.com>
2017-10-31 15:27:01 +0100

    Changed script to check flag.

--- 0ctf_momo_3/reverse.py
+++ 0ctf_momo_3/reverse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from pwn import *
+import time
 import os
 
 f = open('breakpoints' ,'r')
@@ -9,17 +10,22 @@ f.close()
 commands += 'run < input\n'
 commands += 'python exec(open("commands.py").read())\n'
 
-
 log.info('Starting reverse engineering...')
 momo = process('./momo')
 gdb.attach(momo, commands)
-log.info('Reverse engineering done...')
+log.success('Reverse engineering done...')
+
+read = False
+while not read:
+    try:
+        f = open('flag', 'r')
+        flag = f.read()
+        f.close()
+        read = True
+    except:
+        time.sleep(0.5)
 
 log.info('Running program with input "flag":')
 os.system("./momo < flag")
 
-f = open('flag', 'r')
-flag = f.read()
-f.close()
-
-log.info('Flag found: %s', flag)
+log.success('Flag found: %s', flag)
